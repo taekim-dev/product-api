99f9ee31c029e230426555b5e7d9933ea2a30eee
Tae Kim <taekim.dev@gmail.com>
2023-05-04 01:40:54 +0800

    add CORS limit

--- ProductAPI/settings.py
+++ ProductAPI/settings.py
@@ -126,10 +126,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_ALLOW_ALL = True
 
-#CORS_ALLOWED_ORIGINS = [
-#    'https://click-and-collect2.netlify.app',
-#    'http://localhost:3000',
-#    'http://127.0.0.1:3000'
-#]
+CORS_ALLOWED_ORIGINS = [
+    'https://click-and-collect2.netlify.app',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
