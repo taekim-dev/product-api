1b6c13ee05bbf9d49e4741f2feaf8bcf3fd4f1aa
Tae Kim <taekim.dev@gmail.com>
2023-05-04 00:20:25 +0800

    cors allow all for now

--- ProductAPI/settings.py
+++ ProductAPI/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 #CORS_ALLOWED_ORIGINS = [
 #    'https://click-and-collect2.netlify.app',
